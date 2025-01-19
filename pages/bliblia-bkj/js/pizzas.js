4e3eb97b47eb3894ac5e0fd1e11714049a27eeaf
Cleito Gomes <127799139+cltg01@users.noreply.github.com>
2025-01-19 15:18:21 -0300

    Update pizzas.js

--- pages/bliblia-bkj/js/pizzas.js
+++ pages/bliblia-bkj/js/pizzas.js
@@ -1,6 +1,6 @@
 let pizzaJson = [
-    {id:1, name:'Biblia BKJ', img:'/pages/bliblia-bkj/imagens/biblia-bkj-preta.png', price:20.19, sizes:['P', 'D', 'C'], description:'Bíblias exclusivas com capas personalizadas, perfeitas para presentear ou tornar seu momento de leitura ainda mais especial.'},
-    {id:2, name:'Biblia BKJ', img:'/pages/bliblia-bkj/imagens/biblia-bkj-bege.png', price:18.00, sizes:['P', 'D', 'C'], description:'Bíblias exclusivas com capas personalizadas, perfeitas para presentear ou tornar seu momento de leitura ainda mais especial.'},
-    {id:3, name:'Biblia BKJ', img:'/pages/bliblia-bkj/imagens/biblia-bkj-madeira.png', price:17.45, sizes:['P', 'D', 'C'], description:'Bíblias exclusivas com capas personalizadas, perfeitas para presentear ou tornar seu momento de leitura ainda mais especial.'},
+    {id:1, name:'Biblia BKJ', img:'../pages/bliblia-bkj/imagens/biblia-bkj-preta.png', price:20.19, sizes:['P', 'D', 'C'], description:'Bíblias exclusivas com capas personalizadas, perfeitas para presentear ou tornar seu momento de leitura ainda mais especial.'},
+    {id:2, name:'Biblia BKJ', img:'../pages/bliblia-bkj/imagens/biblia-bkj-bege.png', price:18.00, sizes:['P', 'D', 'C'], description:'Bíblias exclusivas com capas personalizadas, perfeitas para presentear ou tornar seu momento de leitura ainda mais especial.'},
+    {id:3, name:'Biblia BKJ', img:'../pages/bliblia-bkj/imagens/biblia-bkj-madeira.png', price:17.45, sizes:['P', 'D', 'C'], description:'Bíblias exclusivas com capas personalizadas, perfeitas para presentear ou tornar seu momento de leitura ainda mais especial.'},
     ];
 
