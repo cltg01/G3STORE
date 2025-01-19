d62aa7d6f99f9332eff233e3b788697a992192b8
Cleito Gomes <127799139+cltg01@users.noreply.github.com>
2025-01-19 15:36:49 -0300

    Update pizzas.js

--- pages/biblia-acf/js/pizzas.js
+++ pages/biblia-acf/js/pizzas.js
@@ -1,5 +1,5 @@
 let pizzaJson = [
-    {id:1, name:'Biblia ACF', img:'../../imagens/biblia-acf-capa-dura.png', price:20.19, sizes:['P', 'D', 'C'], description:'Bíblias exclusivas com capas personalizadas, perfeitas para presentear ou tornar seu momento de leitura ainda mais especial.'},
+    {id:1, name:'Biblia ACF', img:'/imagens/biblia-acf-capa-dura.png', price:20.19, sizes:['P', 'D', 'C'], description:'Bíblias exclusivas com capas personalizadas, perfeitas para presentear ou tornar seu momento de leitura ainda mais especial.'},
     {id:2, name:'Biblia ACF', img:'imagens/biblia-acf-flor.png', price:18.00, sizes:['P', 'D', 'C'], description:'Bíblias exclusivas com capas personalizadas, perfeitas para presentear ou tornar seu momento de leitura ainda mais especial.'},
     {id:3, name:'Biblia ACF', img:'imagens/biblia-acf-paz.png', price:17.45, sizes:['P', 'D', 'C'], description:'Bíblias exclusivas com capas personalizadas, perfeitas para presentear ou tornar seu momento de leitura ainda mais especial.'},
     ];
