a6165be10883c51d2e8b768d059005b1a54b8afb
Cleito Gomes <127799139+cltg01@users.noreply.github.com>
2025-01-19 15:52:46 -0300

    Update pizzas.js

--- pages/biblia-crianca/js/pizzas.js
+++ pages/biblia-crianca/js/pizzas.js
@@ -1,5 +1,5 @@
 let pizzaJson = [
-    {id:1, name:'Biblia 3 Palavrinhas', img:'/pages/biblia-crianca/imagens/biblia-crianca-3palavrinha.png', price:20.19, sizes:['P', 'D', 'C'], description:'Bíblias exclusivas com capas personalizadas, perfeitas para presentear ou tornar seu momento de leitura ainda mais especial.'},
+    {id:1, name:'Biblia 3 Palavrinhas', img:'imagens/biblia-crianca-3palavrinha.png', price:20.19, sizes:['P', 'D', 'C'], description:'Bíblias exclusivas com capas personalizadas, perfeitas para presentear ou tornar seu momento de leitura ainda mais especial.'},
     
 ];
 
